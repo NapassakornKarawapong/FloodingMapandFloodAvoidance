4c293c2a2d4f573c13021f48bb0a3867880d8167
Napassakorn Karawapong <45187742+NapassakornKarawapong@users.noreply.github.com>
2021-05-04 23:54:05 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
-import './components/floodingMap'
-import {Navbar, Nav} from 'react-bootstrap'
+import './floodingMap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './floodAvoidance'
 
 function App() {
   return (
@@ -9,19 +9,28 @@ function App() {
       {/* <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Project II</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home"><h4>Home</h4></Nav.Link>
           <Nav.Link href="/floodingMap"><h4>Flooding Map</h4></Nav.Link>
           <Nav.Link href="/floodAvoidance"><h4>Flood Avoidance</h4></Nav.Link>
         </Nav>
-      </Navbar> */}
+      </Navbar>
+      <floodingMap></floodingMap>
+      <floodAvoidance></floodAvoidance> */}
+
           <iframe 
           width="100%" 
-          height="600" 
+          height="660" 
           src="https://app.powerbi.com/view?r=eyJrIjoiZGQ3ZmU5ZTctNzgyOS00NjQ2LTgwOTktMDIwM2YyZjZkOTU1IiwidCI6IjhlNjM0Z
           TY3LTlkNjYtNDZkMi1hNTI5LWUxYjcwOGM1ZDhiYyIsImMiOjEwfQ%3D%3D&pageName=ReportSection2b6cb3ba55a41534e5c7" 
           frameborder="0" 
           allowFullScreen="true">
           </iframe>
+          <iframe 
+          width="100%" 
+          height="660" 
+          src="https://app.powerbi.com/view?r=eyJrIjoiM2QxZGUyMmUtMjM0ZS00ZmZiLWJkZDMtMWYwMjQ5NTM0MzYwIiwidCI6IjhlNjM0Z
+          TY3LTlkNjYtNDZkMi1hNTI5LWUxYjcwOGM1ZDhiYyIsImMiOjEwfQ%3D%3D&pageName=ReportSection52d5c3a96ca10e8ea26b" 
+          frameborder="0" 
+          allowFullScreen="true"/>
     </div>
   )
 }
