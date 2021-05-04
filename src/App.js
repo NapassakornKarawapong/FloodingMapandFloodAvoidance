2cc304862d7218244d3a36b1de4591675b9e6e6d
Napassakorn Karawapong <45187742+NapassakornKarawapong@users.noreply.github.com>
2021-05-05 00:22:00 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
-      <iframe 
-          width="100%" 
-          height="660" 
+      <iframe width="100%" height="660" 
           src="https://app.powerbi.com/view?r=eyJrIjoiZGQ3ZmU5ZTctNzgyOS00NjQ2LTgwOTktMDIwM2YyZjZkOTU1IiwidCI6IjhlNjM0Z
           TY3LTlkNjYtNDZkMi1hNTI5LWUxYjcwOGM1ZDhiYyIsImMiOjEwfQ%3D%3D&pageName=ReportSection2b6cb3ba55a41534e5c7" 
-          frameborder="0" 
-          allowFullScreen="true">
+          frameborder="0" allowFullScreen="true">
       </iframe>
-      <iframe 
-          width="100%" 
-          height="660" 
+      <iframe width="100%" height="660"
           src="https://app.powerbi.com/view?r=eyJrIjoiM2QxZGUyMmUtMjM0ZS00ZmZiLWJkZDMtMWYwMjQ5NTM0MzYwIiwidCI6IjhlNjM0Z
           TY3LTlkNjYtNDZkMi1hNTI5LWUxYjcwOGM1ZDhiYyIsImMiOjEwfQ%3D%3D&pageName=ReportSection52d5c3a96ca10e8ea26b" 
-          frameborder="0" 
-          allowFullScreen="true">
+          frameborder="0" allowFullScreen="true">
       </iframe>
     </div>
   )
